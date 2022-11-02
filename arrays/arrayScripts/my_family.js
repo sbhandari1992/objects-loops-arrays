18e0ef98d82fc4de7dad5dcfb38a1a1368192434
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-02 10:58:14 -0400

    exercise-1 for array

---
+++ arrays/arrayScripts/my_family.js
@@ -0,0 +1,6 @@
+let myFamily = ['Father','Mother','Sister','Brother','Grandma','Grandpa'];
+let myFamilyArrayLeangth = myFamily.length;
+
+for (let i = 0; i < myFamilyArrayLeangth; i++) {
+    console.log(myFamily[i]);
+}
