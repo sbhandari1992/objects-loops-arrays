01668d20916782fe831c26eddc52b4fc1896902d
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-03 21:39:02 -0400

    food.js degubg

--- arrays/arrayScripts/foods.js
+++ arrays/arrayScripts/foods.js
@@ -1,21 +1,30 @@
 let lunch = [
-    {item: "Steak Fajitas", price: 1},
-    {item: "Chips and Guacamole", price: 2},
-    {item: "Sweet Tea", price: 3}
+    {item: "Steak Fajitas", price: 10},
+    {item: "Chips and Guacamole", price: 20},
+    {item: "Sweet Tea", price: 30}
 ];
 let lunchLength = lunch.length;
 let sum = 0;
 let taxRate = 0.08;
 let tipsRate = 0.18;
+let totWithTax;
+let tipsAmt;
+let subTotal;
+let taxAmt;
 
 for (let i = 0; i < lunchLength; i++) {
     sum += (lunch[i].price);
-    console.log(`total without tax and tips is ${sum}`);
-    let totWithTax = (sum + (sum * taxRate));
-    console.log(`total amount with tax is ${totWithTax}`);
-    let tipsAmt = totWithTax * tipsRate;
-    console.log(`tips amount is ${tipsAmt}`);
-    let subTotal = sum + totWithTax + tipsAmt;
-    console.log(`Your subtotal is ${subTotal}`);
+    taxAmt = sum * taxRate;
+    totWithTax = sum + taxAmt;
+    tipsAmt = totWithTax * tipsRate;
+    subTotal = totWithTax + tipsAmt;
 
 }
+console.log(`Total without tax and tips is $${sum}`);
+console.log(`Tax amount is $${taxAmt}`);
+console.log(`Tips amount is $${tipsAmt}`);
+
+console.log(`Your subtotal is $${subTotal}`);
+
+
+
