daa83bfdfb5d285338a445158a893992de703deb
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-06 10:19:27 -0500

    try sorting with lower and uppercase in order

--- arrays/sorting-array/product_sorting.js
+++ arrays/sorting-array/product_sorting.js
@@ -1,21 +1,21 @@
 let products = [
     {product: "Swedish Fish", price: 3.79},
-    {product: "Gummy Worms", price: 5.79},
+    {product: "gummy Worms", price: 5.79},
     {product: "Plain M&Ms", price: 2.89},
     {product: "Peanut M&Ms", price: 2.89}
     
 ];
+console.log(products.product);
 
 products.sort(function(a,b){
-    if (a.product < b.product) return -1;
+    if (a.product.toUpperCase() < b.product.toUpperCase()) return -1;
     else if (a.product == b.product) return 0;
     else return 1
 
 });
-// console.log(products);
 let productsLength = products.length;
 console.log(productsLength);
 for (let i = 0; i < productsLength; i++) {
-    console.log(`accending product title ${i + 1} is ${products[i].product}`);
+    console.log(`----------- accending product title ${i + 1} is ${products[i].product}`);
 
 }
